9923349aa2276d279b4ad71e5709ec839796bf90
daniel-akinyemi <danielakinyemi1999@gmail.com>
2024-02-14 15:05:30 +0100

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from './pages/signIn'
 import SignOut from './pages/signOut'
 import About from './pages/about'
 import Profile from './pages/profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path='/sign-in' element={<SignIn/>}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1 className='font-bold text-sm sm:text-xl'>
+            <span className='text-slate-500'>Sahand</span>
+
+            <span className='text-slate-700'>Estate</span>
+        </h1>
+    </header>
+  )
+}
+
+export default Header
