77bd226dc5b42ce2061af7ea7b4fc45963cd00ad
daniel-akinyemi <danielakinyemi1999@gmail.com>
2024-02-14 15:30:23 +0100

    header component updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import {FaSearch} from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <header>
-        <h1 className='font-bold text-sm sm:text-xl'>
-            <span className='text-slate-500'>Sahand</span>
+    <header className="bg-slate-200 shadow-md flex">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl">
+          <span className="text-slate-500">Sahand</span>
 
-            <span className='text-slate-700'>Estate</span>
+          <span className="text-slate-700">Estate</span>
         </h1>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+          <FaSearch className='text-slate-600'/>
+        </form>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
