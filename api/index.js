222e7ad0e878007793ee4e726789dd1265326ac7
daniel-akinyemi <danielakinyemi1999@gmail.com>
2024-02-17 13:04:52 +0100

    create: midleware and function

--- api/index.js
+++ api/index.js
@@ -18,3 +18,13 @@ app.listen(3001,()=>{console.log(`listening on port localhost:${3001}`)})
 app.use(express.json())
 app.use("/api/user",UserRouter)
 app.use("/api/auth",authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
