65cb4520a7917361233811ca80a381138a3e5357
daniel-akinyemi <danielakinyemi1999@gmail.com>
2024-02-17 18:11:06 +0100

    complete: sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signin = async(req,res,next) =>{
         if (!validPassword) return next(errorHandler(401, 'Invalid password!'))
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
         const {password:pass, ...userInfo} = validUser._doc
-        res.cookie('access_token', token, {httpOnly: true}).status(200).json(validUser)
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(userInfo)
 
     } catch (error) {
         next(error)
--- client/src/pages/signIn.jsx
+++ client/src/pages/signIn.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
+  const [formData,setFormData] = useState({})
+  const [error,setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+  const handleChange = (e)=>{
+    setFormData({...formData,
+    [e.target.id]:e.target.value
+    })
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    try {
+      setIsLoading(true)
+    const res = await fetch('/api/auth/signin', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+
+      },
+      body: JSON.stringify(formData)
+    })
+    const data = await res.json()
+    if(data.success === false){
+      setIsLoading(false)
+      setError(data.message)      
+      return
+    }
+    setIsLoading(false)
+    console.log(data);
+    setError(error.message)
+    navigate('/')
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+    
+  }
+  console.log(formData)
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign IN</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="email" placeholder='email' className="border p-3 rounded-lg" id='email' onChange={handleChange}/>
+        <input type="password" placeholder='password' className="border p-3 rounded-lg" id='password' onChange={handleChange}/>
+        <button disabled={isLoading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{ isLoading ? 'Loading' : 'Sign In'}</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>dont have an account?</p>
+        <Link to={"/sign-up"}>Sign up</Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      </div>
   )
 }
 
